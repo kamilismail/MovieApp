bbd52efa594352c9bc9cbb5b66dd031a69694f5d
Kamil Ismail <ismail.katowice@gmail.com>
2018-07-12 19:43:10 +0200

    założenie projektu backend'owego

---
+++ MovieApp/src/main/java/com/KamilIsmail/MovieApp/MovieAppApplication.java
@@ -0,0 +1,12 @@
+package com.KamilIsmail.MovieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppApplication.class, args);
+	}
+}
